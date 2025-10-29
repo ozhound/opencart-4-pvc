1e28f094c02a21eddf5d7caf8576986303f86eb6
ozhound <ozhound@gmail.com>
2025-10-29 13:44:33 +0800

    Refactor namespace and clean up comments in opencart_pvc.php

--- admin/controller/extension/OpencartPvc/module/opencart_pvc.php
+++ admin/controller/extension/OpencartPvc/module/opencart_pvc.php
@@ -1,11 +1,8 @@
 <?php
-namespace Opencart\Admin\Controller\Extension\OpencartPvc\Module;
+namespace Opencart\Extension\OpencartPvc\Admin\Controller\Module;
 
 class OpencartPvc extends \Opencart\System\Engine\Controller {
-
-    /* install(): create DB table only */
     public function install(): void {
-        $this->log->write('PVC install fired');
         $this->db->query("
             CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "product_customer_group` (
                 product_id INT NOT NULL,
@@ -15,13 +12,10 @@ class OpencartPvc extends \Opencart\System\Engine\Controller {
         ");
     }
 
-    /* uninstall(): drop table only */
     public function uninstall(): void {
-
         $this->db->query("DROP TABLE IF EXISTS `" . DB_PREFIX . "product_customer_group`");
     }
 
-    /* index(): stub */
     public function index(): void {
         $this->response->setOutput('PVC module');
     }
