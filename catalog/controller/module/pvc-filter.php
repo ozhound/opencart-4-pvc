f937cf13b26b4240b2c5919f21240eac6c2b5ae6
ozhound <ozhound@gmail.com>
2025-10-29 15:12:51 +0800

    Refactor namespace for PvcFilter controller

--- catalog/controller/module/pvc-filter.php
+++ catalog/controller/module/pvc-filter.php
@@ -1,10 +1,8 @@
 <?php
-namespace Opencart\Catalog\Controller\Module;
+namespace Opencart\Catalog\Controller\Extension\OpencartPvc\Module;
 
 class PvcFilter extends \Opencart\System\Engine\Controller {
 
-    /* event: catalog/controller/*/before
-       stores customer_group_id for later filtering */
     public function before(&$route, &$args): void {
         $gid = 0;
         if ($this->customer->isLogged()) {
@@ -13,10 +11,7 @@ class PvcFilter extends \Opencart\System\Engine\Controller {
         $this->registry->set('pvc_group_id', $gid);
     }
 
-    /* event: catalog/model/catalog/product/getProducts/before
-       appends visibility restriction to product filter */
     public function apply(&$route, &$args): void {
-
         $gid = (int)($this->registry->get('pvc_group_id') ?? 0);
 
         if (!isset($args[0]) || !is_array($args[0])) {
